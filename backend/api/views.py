209e945349c8ef3cee7bc862890c0a66988530ad
Kaito <umut.yildiz@tedu.edu.tr>
2025-04-02 10:14:03 +0300

    I think this is final

--- backend/api/views.py
+++ backend/api/views.py
@@ -44,7 +44,6 @@ class CreateUserView(generics.CreateAPIView):
     permission_classes = [AllowAny]
 
 class CustomLoginView(APIView):
-    permission_classes = [AllowAny]
 
     def post(self, request):
         username = request.data.get("username")
